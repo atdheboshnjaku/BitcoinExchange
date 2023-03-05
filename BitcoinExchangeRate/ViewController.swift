dc40fa642954745157df7b00f2619c5d010a50b7
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2023-03-05 10:57:51 +0100

    Initial Commit

---
+++ BitcoinExchangeRate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitcoinExchangeRate
+//
+//  Created by Atdhe Boshnjaku on 3/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BitcoinExchangeRateUITests/BitcoinExchangeRateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BitcoinExchangeRateUITestsLaunchTests.swift
+//  BitcoinExchangeRateUITests
+//
+//  Created by Atdhe Boshnjaku on 3/5/23.
+//
+
+import XCTest
+
+final class BitcoinExchangeRateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
